af7797080e4d925dc0eca20b520a7e24289c2928
Torbjörn Rathsman <milasudril@gmail.com>
2024-09-28 16:29:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Input Activity Watchdog
+
+## Example
+
+Use `glxgears` as a screensaver, that starts after 60 seconds of inactivity
+
+./iaw.py 60 glxgears -fullscreen
---
+++ iaw.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import inotify.adapters
+import sys
+import time
+import subprocess
+
+def listen_on_dev_input(timeout, command):
+	i = inotify.adapters.Inotify(block_duration_s = timeout)
+	i.add_watch('/dev/input')
+	start = time.monotonic_ns()
+	proc = None
+	for event in i.event_gen():
+		now = time.monotonic_ns()
+		if event == None and now - start >= timeout*1e9:
+			if proc == None:
+				proc = subprocess.Popen(command)
+		else:
+			if proc != None:
+				proc.terminate()
+				proc = None
+			start = now
+
+def main(argv):
+	listen_on_dev_input(timeout = float(argv[1]), command = argv[2:])
+
+if __name__ == '__main__':
+	main(sys.argv)
